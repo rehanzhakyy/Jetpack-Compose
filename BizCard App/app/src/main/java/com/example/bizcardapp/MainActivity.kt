aba80ef47fa40ade6513a9ef2a4b16cc6c667039
raelhan <hannn9491@gmail.com>
2023-11-02 13:28:55 +0700

    Creating a Card

--- BizCard App/app/src/main/java/com/example/bizcardapp/MainActivity.kt
+++ BizCard App/app/src/main/java/com/example/bizcardapp/MainActivity.kt
@@ -3,15 +3,25 @@ package com.example.bizcardapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.bizcardapp.ui.theme.BizCardAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,11 +43,25 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CreateBizCard() {
-    Surface(modifier = Modifier.fillMaxWidth()
+    Surface(modifier = Modifier
+        .fillMaxWidth()
         .fillMaxHeight()) {
+        Card(modifier = Modifier.width(200.dp)
+                .height(390.dp)
+                .width(12.dp),
+            RoundedCornerShape(corner = CornerSize(15.dp)),
+            colors = CardDefaults.cardColors( // inisialisasi untuk background color
+                containerColor = Color.Green
+            ),
+            elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)) {
+
+        }
 
     }
 }
+
+
+
 
 @Preview(showBackground = true)
 @Composable
