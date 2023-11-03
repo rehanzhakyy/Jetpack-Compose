5c8660fe3a7fcb46b27230b3242aec78c5bb8fc1
raelhan <hannn9491@gmail.com>
2023-11-03 11:07:30 +0700

    Adding the Profile Image

--- BizCard App/app/src/main/java/com/example/bizcardapp/MainActivity.kt
+++ BizCard App/app/src/main/java/com/example/bizcardapp/MainActivity.kt
@@ -3,13 +3,17 @@ package com.example.bizcardapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
@@ -17,12 +21,17 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.bizcardapp.ui.theme.BizCardAppTheme
+import androidx.compose.material3.Card
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -46,16 +55,28 @@ fun CreateBizCard() {
     Surface(modifier = Modifier
         .fillMaxWidth()
         .fillMaxHeight()) {
-        Card(modifier = Modifier.width(200.dp)
-                .height(390.dp)
-                .width(12.dp),
-            RoundedCornerShape(corner = CornerSize(15.dp)),
-            // inisialisasi untuk background color
-            colors = CardDefaults.cardColors(
-                containerColor = Color.Green
-            ),
+        Card(modifier = Modifier
+            .width(200.dp)
+            .height(390.dp)
+            .width(12.dp),
+            shape = RoundedCornerShape(corner = CornerSize(15.dp)),
             // inisialiasi untuk elevation
-            elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)) {
+            elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)){
+
+            Surface(modifier = Modifier
+                .size(150.dp)
+                .padding(5.dp),
+                    shape = CircleShape,
+                    border = BorderStroke(0.5.dp, Color.LightGray),
+                    shadowElevation = 4.dp,
+                    color = Color.LightGray) {
+
+                Image(painter = painterResource(id = R.drawable.zaki),
+                    contentDescription = "Profile Image",
+                    modifier = Modifier.size(135.dp),
+                    contentScale = ContentScale.Crop)
+
+            }
 
         }
 
