234d0273e94f17e50d74a7fadea6b1061ac106cc
raelhan <hannn9491@gmail.com>
2023-11-16 13:13:56 +0700

    Adding a Column and a Divider

--- BizCard App/app/src/main/java/com/example/bizcardapp/MainActivity.kt
+++ BizCard App/app/src/main/java/com/example/bizcardapp/MainActivity.kt
@@ -6,6 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -29,6 +31,9 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.bizcardapp.ui.theme.BizCardAppTheme
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardElevation
+import androidx.compose.material3.Divider
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 
@@ -52,43 +57,57 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CreateBizCard() {
-    Surface(modifier = Modifier
-        .fillMaxWidth()
-        .fillMaxHeight()) {
-        Card(modifier = Modifier
-            .width(200.dp)
-            .height(390.dp)
-            .width(12.dp),
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()) {
+        Card(
+            modifier = Modifier
+                .width(200.dp)
+                .height(390.dp)
+                .padding(12.dp),
             shape = RoundedCornerShape(corner = CornerSize(15.dp)),
-            // inisialiasi untuk elevation
-            elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)){
+            elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)) {
 
-            Surface(modifier = Modifier
-                .size(150.dp)
-                .padding(5.dp),
-                    shape = CircleShape,
-                    border = BorderStroke(0.5.dp, Color.LightGray),
-                    shadowElevation = 4.dp,
-                    color = Color.LightGray) {
-
-                Image(painter = painterResource(id = R.drawable.zaki),
-                    contentDescription = "Profile Image",
-                    modifier = Modifier.size(135.dp),
-                    contentScale = ContentScale.Crop)
-
+            Column(modifier = Modifier.height(300.dp),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally) {
+                CreatedImageProfile()
+                Divider()
             }
 
+
         }
+    }
+
+}
+
+@Composable
+private fun CreatedImageProfile(modifier: Modifier = Modifier) {
+    Surface(
+        modifier = Modifier
+            .size(150.dp)
+            .padding(5.dp),
+        shape = CircleShape,
+        border = BorderStroke(0.5.dp, Color.LightGray),
+        shadowElevation = 4.dp,
+        color = Color.LightGray
+    ) {
+
+        Image(
+            painter = painterResource(id = R.drawable.zaki),
+            contentDescription = "Profile Image",
+            modifier = Modifier.size(135.dp),
+            contentScale = ContentScale.Crop
+        )
 
     }
 }
 
 
-
-
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun DefaultPreview() {
     BizCardAppTheme {
         CreateBizCard()
     }
